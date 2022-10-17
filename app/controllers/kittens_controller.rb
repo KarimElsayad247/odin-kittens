6c856bc9e316ac5af44f9a97f84df438caa37ea6
KarimElsayad247 <karimelsayad1999@gmail.com>
2022-10-17 18:26:13 +0200

    Finish json tasks

--- app/controllers/kittens_controller.rb
+++ app/controllers/kittens_controller.rb
@@ -1,10 +1,20 @@
 class KittensController < ApplicationController
   def index
     @kittens = Kitten.all
+
+    respond_to do |format|
+      format.html # index.html.erb
+      format.json { render json: @kittens }
+    end
   end
 
   def show
     @kitten = Kitten.find(params[:id])
+
+    respond_to do |format|
+      format.html # show.html.erb
+      format.json { render json: @kitten }
+    end
   end
 
   def new
